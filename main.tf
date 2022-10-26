4a573aff475d863884f74e22ef07ecdcd9386422
Vaibhav Ghosalkar <vghosalkar2311@gmail.com>
2022-10-26 16:30:53 -0400

    Main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = "AKIATMI2CI3RVMRHS7HP"
   secret_key = "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"
 
