efc48cce8dffcc6a33970a01dea5b935fb4532f3
Vaibhav Ghosalkar <vghosalkar2311@gmail.com>
2022-10-26 22:04:37 -0400

    testing

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 provider "aws" {
   # Configuration options
   region     = "us-east-1"
-  access_key = "AKIATMI2CI3RVMRHS7HP"
-  secret_key = "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"
-
 }
 
 resource "aws_iam_user" "iamuer" {
 
-  for_each = toset(["user1", "user2", "user3"])
+  for_each = toset(["user1", "user2", "user3","user4"])
   name     = each.key
 }
