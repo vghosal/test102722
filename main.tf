c4475bfa1b9c7d9027e695574a73eb13d24da1dd
Vaibhav Ghosalkar <vghosalkar2311@gmail.com>
2022-10-27 16:48:43 -0400

    modules testing

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  # Configuration options
-  region     = "us-east-1"
-}
-
-resource "aws_iam_user" "iamuer" {
-
-  for_each = toset(["user1", "user2", "user3","user4"])
-  name     = each.key
+module "s3bucket" {
+  source = "./modules/s3"
 }
